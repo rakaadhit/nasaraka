79a7e83280e0864d24d1f64c88e8f3ede7c2a97a
rakaadhit <48428899+rakaadhit@users.noreply.github.com>
2019-04-16 11:43:39 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=pS102UKiX8grv7YCFBoPdaNPzPcnyZsUJSzNQWJI";
         
         $("#loadImage").click(function(){
             $.ajax({
